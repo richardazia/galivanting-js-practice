fb7d1555a1a93a90e6568ad26aa5f7dfe85cfeb3
Richard Azia <richard@main-vision.com>
2021-12-23 12:58:09 +0100

    add global scope

--- script.js
+++ script.js
@@ -53,3 +53,34 @@
 // var warrior; // warrior is declared here
 
 // var warrior3; // will show undefined if it is declared and called at the same time -- warrior3 = 'viking'; --
+
+// 4. Global Variables
+
+const warrior = {
+    name: 'Jujin Take',
+    type: 'Ninja',
+    weapon: 'Shuriken',
+    agility: 79
+} 
+// this is globally scoped
+
+//Globally scoped function
+const screamWarrior = () => {
+    let warrior2 = 'samurai';
+
+    warrior3 = 'Viking';
+
+    return {
+        shootWarrior: () => {
+            return console.log(warrior, warrior2);
+        }
+    }
+}
+
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+console.log(warrior, warrior3);
+
+// add 'use sctrict'; to prevent viking from working. 
