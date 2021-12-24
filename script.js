0da646f39e81eceec7ed6c87f899ec4596a46c24
Richard Azia <richard@main-vision.com>
2021-12-24 16:35:11 +0100

    add ppe const

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const school = () => {
 }
 
 console.log(school());
+
+// global variable
+
+var numberOfBoxes = 200;
+
+const ppe = () => {
+    // local variable
+    let masksPerBox = 50;
+
+    // second variable
+    let totalMasks = numberOfBoxes * masksPerBox;
+
+    return `Our collection of masks is ${totalMasks} strong`;
+}
+
+console.log(ppe());
