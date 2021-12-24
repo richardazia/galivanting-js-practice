a3890d49b56dcfa570081158893052c87a2edcb1
Richard Azia <richard@main-vision.com>
2021-12-24 12:49:00 +0100

    Play with local variables

--- script.js
+++ script.js
@@ -1,110 +1,20 @@
-// 1. Scope
-// var warrior = 'ninja';
+const couacBird = () => {
+    var bird1 = 'Ducks';
+    var bird2 = 'Seagulls';
 
-// const screamWarrior = () => {
-//     let warrior2 = 'Samurai';
-//     console.log(warrior, warrior2);
-// }
-
-// screamWarrior();
-
-// console.log(warrior, warrior2);
-
-//2. Closure
-
-// var warrior = 'ninja';
-
-// const screamWarrior = () => {
-//     let warrior2 = 'Samurai';
-//     return {
-//         shootWarrior: () => {
-//             return console.log(warrior, warrior2);
-//         }
-//     }
-// }
-
-// const newWarrior =  screamWarrior();
-
-// newWarrior.shootWarrior();
-
-// console.log(warrior, warrior2);
-
-// 3. Hoisting
-
-// warrior = 'ninja'; // warrior is defined here
-// warrior3 = 'viking'; 
-
-// const screamWarrior = () => {
-//     let warrior2 = 'Samurai';
-//     // wrapped in a function now is a closure
-//     return {
-//         shootWarrior: () => {
-//             return console.log(warrior, warrior2);
-//         }
-//     }
-// }
-
-// const newWarrior =  screamWarrior();
-
-// newWarrior.shootWarrior();
-
-// console.log(warrior, warrior3);
-
-// var warrior; // warrior is declared here
-
-// var warrior3; // will show undefined if it is declared and called at the same time -- warrior3 = 'viking'; --
-
-// 4. Global Variables
-
-const warrior = {
-    name: 'Jujin Take',
-    type: 'Ninja',
-    weapon: 'Shuriken',
-    agility: 79
-} 
-
-const knight = {
-    name: 'Richard',
-    type: 'Crusader',
-    weapon: 'Sword',
-    agility: 67
-}
-// this is globally scoped
-
-//Globally scoped function
-const screamWarrior = () => {
-    let warrior2 = 'samurai';
-
-    warrior3 = 'Viking';
-
-    return {
-        shootWarrior: () => {
-            return console.log(warrior, warrior2);
-        }
-    }
+    return `Our birds are ${bird1} and ${bird2}.`;
 }
 
-// console.log(knight3) knight 3 is not defined globally yet
+const couacBird2 = () => {
+    var bird1 = 'Herons';
+    var bird2 = 'Eagles';
 
-const screamKnight = () => {
-    let knight2 = 'Dragoon';
-
-    knight3 = 'trebuchet';
-
-    return {
-        shootKnight: () => {
-            return console.log(knight, knight2)
-        }
-    }
+    return `Our birds are ${bird1} and ${bird2}.`;
 }
 
-const newWarrior = screamWarrior();
-const newKnight = screamKnight();
+console.log(couacBird());
+console.log(couacBird2());
 
-newWarrior.shootWarrior();
-newKnight.shootKnight();
-console.log(warrior, warrior3);
-console.log(knight, knight3); //knight3 is defined globally from within the function. 
-
-
-// add 'use sctrict'; to prevent viking from working. 
+console.log(couacBird(), couacBird2());
+// bird 1 and 2 are out of scope as they are declared locally. 
+console.log(bird1, bird2);
