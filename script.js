fccbb5e13a61cddfe4d7bb95ccec543a9eda5fbc
Richard Azia <richard@main-vision.com>
2021-12-24 12:30:31 +0100

    add knight, knight 2 and 3 for global practice

--- script.js
+++ script.js
@@ -62,6 +62,13 @@ const warrior = {
     weapon: 'Shuriken',
     agility: 79
 } 
+
+const knight = {
+    name: 'Richard',
+    type: 'Crusader',
+    weapon: 'Sword',
+    agility: 67
+}
 // this is globally scoped
 
 //Globally scoped function
@@ -77,10 +84,27 @@ const screamWarrior = () => {
     }
 }
 
+// console.log(knight3) knight 3 is not defined globally yet
+
+const screamKnight = () => {
+    let knight2 = 'Dragoon';
+
+    knight3 = 'trebuchet';
+
+    return {
+        shootKnight: () => {
+            return console.log(knight, knight2)
+        }
+    }
+}
+
 const newWarrior = screamWarrior();
+const newKnight = screamKnight();
 
 newWarrior.shootWarrior();
-
+newKnight.shootKnight();
 console.log(warrior, warrior3);
+console.log(knight, knight3); //knight3 is defined globally from within the function. 
+
 
 // add 'use sctrict'; to prevent viking from working. 
