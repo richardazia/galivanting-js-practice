cfdebbedfc681461f1bc6b0ef8079791b734c253
Richard Azia <richard@main-vision.com>
2021-12-27 12:05:39 +0100

    use JSON parse to duplicate object

--- strings.js
+++ strings.js
@@ -11,3 +11,19 @@ var myLocation = "Spain", myOtherLocation = "Switzerland";
 myLocation === myOtherLocation; // false
 
 myLocation !== myOtherLocation; // true
+
+
+Book = {
+    title: 'Last Book to Woodstock',
+    author: 'Colin Dexter',
+    format: 'audible',
+    rating: 'good'
+}
+
+Book2 = JSON.parse(JSON.stringify(Book));
+
+Book3 = JSON.parse(JSON.stringify(Book)); 
+
+console.log(Book2);
+
+console.log(Book3);
