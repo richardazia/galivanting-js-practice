d2f4ec5e34b813e72965259c7c91c5b53bfd0cfb
Richard Azia <richard@main-vision.com>
2021-12-29 17:25:18 +0100

    Add conditional if

--- strings.js
+++ strings.js
@@ -41,3 +41,36 @@ console.log(animal1 === 'chicken' && animal2 === 'goose' && animal3 === 'Chamois
 console.log(animal1 === 'bear' || animal2 === 'mountain lion'); // because at least one is true
 
 console.log(animal1 === 'tricycle' && animal2 === 'e-bike'); // both are false so the result is false. 
+
+// Conditionals: if
+
+var answer = window.confirm("Click Ok, get true. Click cancel, get false.");
+answer;
+
+if (answer === true) {
+    console.log("You clicked True!");
+} else {
+    console.log("You said something else");
+}
+
+var answer = window.prompt("Type yes, NO or Maybe. Then Click Ok.");
+if (answer === "Yes") {
+    console.log("You wrote yes");
+} else if (answer === "Maybe") {
+    console.log("You wrote Maybe, but not baby");
+} else if (answer === "NO") {
+    console.log("You yelled 'NO'");
+} else {
+    console.log("Check for typos");
+}
+
+var pandemic = window.prompt("Type Mask, Self-Isolation or Vaccinations, then click OK");
+if (pandemic === "Mask") {
+    console.log("Thank you for wearing a mask");
+} else if (pandemic === "Self-Isolation") {
+    console.log("Thank you for self-isolating");
+} else if (pandemic === "Vaccinations") {
+    console.log("Thank you for being vaccinated");
+}   else {
+    console.log("Make more of an effort");
+}
