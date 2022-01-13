ce260420de92ff0679e08f6df40255e9610e7ef6
Richard A <richard@main-vision.com>
2022-01-13 17:29:40 +0100

    failed attempt to read instagram json file

--- instagramJson.js
+++ instagramJson.js
@@ -1,10 +1,28 @@
-let insagram = () => 
+let instagram = () => 
 fetch('posts_1.json')
 .then(res => res.json())
 .then(data => console.log(data));
-insagram();
+instagram();
 
 // .then(console.log);
 
 // instagram().then((data) => 
 //     console.log(data));
+
+// let igPosts = () => 
+//     instagram()
+//     .then(json =>json.posts)
+//     .then(posts => posts.map(post => post.title))
+//     .then(titles => console.log(titles));
+
+
+
+// igPosts();
+
+let igUris = () => 
+    instagram()
+    .then(json =>json.media)
+    .then(media => media.map(media => media.uri))
+    .then(uris => console.log(uris));
+
+igUris();
