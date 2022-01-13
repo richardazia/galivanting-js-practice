4db921fe7a50a9b902ef49bb020cbbcfbb687413
Richard A <richard@main-vision.com>
2022-01-13 17:41:37 +0100

    unable to read json file this way.

--- instagramJson.js
+++ instagramJson.js
@@ -3,6 +3,8 @@ fetch('posts_1.json')
 .then(res => res.json())
 .then(data => console.log(data));
 instagram();
+console.log(data.json());
+
 
 // .then(console.log);
 
@@ -19,10 +21,10 @@ instagram();
 
 // igPosts();
 
-let igUris = () => 
-    instagram()
-    .then(json =>json.media)
-    .then(media => media.map(media => media.uri))
-    .then(uris => console.log(uris));
+// let igUris = () => 
+//     instagram()
+//     .then(json =>json.media)
+//     .then(media => media.map(media => media.uri))
+//     .then(uris => console.log(uris));
 
-igUris();
+// igUris();
