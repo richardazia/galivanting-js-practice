93d3a87f96b4e6d332afe38b81b396ab9f5084f0
Richard A <richard@main-vision.com>
2022-01-15 17:12:21 +0100

    add io connection to index.js

--- node/index.js
+++ node/index.js
@@ -5,11 +5,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server} = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+    console.log('New browser instance started');
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
