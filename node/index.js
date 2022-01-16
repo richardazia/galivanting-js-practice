74f3dbeffaa13255562dbaff214430d89ec617db
Richard A <richard@main-vision.com>
2022-01-16 11:19:39 +0100

    add homework tasks to find solutions for.

--- node/index.js
+++ node/index.js
@@ -19,3 +19,19 @@ io.on('connection', (socket) => {
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+// Homework#
+// Here are some ideas to improve the application:
+
+// Broadcast a message to connected users when someone connects or disconnects.
+
+// Add support for nicknames.
+
+// Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
+// Add “{user} is typing” functionality.
+
+// Show who’s online.
+
+// Add private messaging.
+
+// Share your improvements!
