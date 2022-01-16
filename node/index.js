a263e97a87f93f1564df125f796f5fb92b102203
Richard A <richard@main-vision.com>
2022-01-16 11:10:46 +0100

    debug index.js

--- node/index.js
+++ node/index.js
@@ -1,29 +1,21 @@
 // Using the Socket IO library and example code from the following tutorial:
 // https://socket.io/get-started/chat
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');  
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-    console.log('user left');
-  });
-  socket.on('chat message', (msg) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on localhost:3000');
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
