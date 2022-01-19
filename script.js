452cb909a9e6991a182a36a8dbd3d3500a9268d5
Richard A <richard@main-vision.com>
2022-01-19 17:35:59 +0100

    add makeadder function

--- script.js
+++ script.js
@@ -244,3 +244,17 @@ myNumbers.forEach(function(number) {
 });
 
 doubleIt = number => number *= 2;
+
+// Closures
+
+function makeAdder(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2));   // returns 10
+console.log(add10(2));  // returns 20
