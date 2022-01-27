599db43cdb4a81531d0ee814df2873a5d58a4d8e
Richard A <richard@main-vision.com>
2022-01-27 17:56:50 +0100

    index.js now runs without modifying images

--- imagemin/index.js
+++ imagemin/index.js
@@ -3,12 +3,12 @@ import imagemin from 'imagemin';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
 
-const files = await imagemin(['img*.{jpg,png}'], {
-	destination: 'img/',
+const files = await imagemin(["*.*}"], {
+	destination: '/img/',
 	plugins: [
 		imageminJpegtran(),
 		imageminPngquant({
 			quality: [0.6, 0.8]
-		})
+		}),
 	]
 });
