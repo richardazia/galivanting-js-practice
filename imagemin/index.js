d11c10a496d4fad2e2d1b366da5847eaf2edca3c
Richard A <richard@main-vision.com>
2022-01-27 17:21:56 +0100

    init package.json

--- imagemin/index.js
+++ imagemin/index.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+
+(async () => {
+    const files = await imagemin(['../img/**.*'], {
+        destination: '../img/min/',
+        plugins: [
+            imageminMozjpeg({
+                quality: 80
+            }),
+            imageminPngquant({
+                quality: [0.6, 0.8]
+            })
+        ]
+    });
+    console.log(files);
+}
+)();
