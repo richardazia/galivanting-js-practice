41d66626ad7b78c21b6faec3d03b6fc75c9e0650
Richard A <richard@main-vision.com>
2022-01-29 11:19:04 +0100

    add const url

---
+++ ajax-json/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=3331%20Erie%20Ave&street2=&city=Cincinnati&state=OH';
+
+console.log(url);
