12cf792af29b92c110add72e847a649b0c8ebd08
Richard A <richard@main-vision.com>
2022-01-29 11:39:50 +0100

    create xml request

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
+//test url
+
 const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=3331%20Erie%20Ave&street2=&city=Cincinnati&state=OH';
 
-console.log(url);
+const createRequest = function(url) {
+    const httpRequest = new XMLHttpRequest(url);
+    httpRequest.addEventListener('readystatechange', (url) => {
+        if (httpRequest.readyState === 4) {
+            console.log(httpRequest.responseText);
+        }
+    });
+    httpRequest.open('GET', url);
+    httpRequest.send();
+};
+createRequest(url);
