0837f3344bd90ab6167ae48e968793ac2602112e
Richard A <richard@main-vision.com>
2022-01-29 11:46:06 +0100

    add create request and xhr2

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -1,8 +1,12 @@
 'use strict';
+// added for xhr to work: see: https://stackoverflow.com/questions/32604460/xmlhttprequest-module-not-defined-found
+
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
 
 //test url
 
-const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=3331%20Erie%20Ave&street2=&city=Cincinnati&state=OH';
+const url = 'https://us-street.api.smartystreets.com/street-address?key=21102174564513388&street=1428%20Post%20Aly&street2=&city=Seattle&state=WA&zipcode=98101&candidates=10&match=invalid';
 
 const createRequest = function(url) {
     const httpRequest = new XMLHttpRequest(url);
