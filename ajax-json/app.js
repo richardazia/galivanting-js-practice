e43009f332ec78ccda77fdc0abfee004b1f0b900
Richard A <richard@main-vision.com>
2022-01-30 10:42:21 +0100

    Handle xhr success and failure

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -8,13 +8,27 @@
 
 const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=1428%20Post%20Aly&street2=&city=Seattle&state=WA&zipcode=98101&candidates=10&match=invalid';
 
+const updateUISuccess = function(data) {
+    console.log(data);
+};
+
+const updateUIError = function(error) {
+    console.log(error);
+};
+
+const responseMethod = function(httpRequest) {
+    if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+            updateUISuccess(httpRequest.responseText);
+        } else {
+            updateUIError(httpRequest.status + ': ' + httpRequest.responseText);
+        }
+    }
+}
+
 const createRequest = function(url) {
     const httpRequest = new XMLHttpRequest(url);
-    httpRequest.addEventListener('readystatechange', (url) => {
-        if (httpRequest.readyState === 4) {
-            console.log(httpRequest.responseText);
-        }
-    });
+    httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest));
     httpRequest.open('GET', url);
     httpRequest.send();
 };
