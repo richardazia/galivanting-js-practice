2a657a495059cd037e77af7ab8fc7ee31ad7de50
Richard A <richard@main-vision.com>
2022-01-30 11:07:03 +0100

    use urlpark to get list of national parks

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -6,6 +6,8 @@
 
 //test url
 
+// For Street addresses
+/*
 const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=1428%20Post%20Aly&street2=&city=Seattle&state=WA&zipcode=98101&candidates=10&match=invalid';
 
 const updateUISuccess = function(data) {
@@ -33,3 +35,35 @@ const createRequest = function(url) {
     httpRequest.send();
 };
 createRequest(url);
+*/
+
+// For National parks
+
+const urlPark = 'https://developer.nps.gov/api/v1/parks?api_key=yourkeyhere';
+
+const updateUISuccess = function(data) {
+    console.log(data);
+};
+
+const updateUIError = function(error) {
+    console.log(error);
+};
+
+const responseMethod = function(httpRequest) {
+    if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+            updateUISuccess(httpRequest.responseText);
+        } else {
+            updateUIError(httpRequest.status + ': ' + httpRequest.responseText);
+        }
+    }
+}
+
+const createRequest = function(url) {
+    const httpRequest = new XMLHttpRequest(url);
+    httpRequest.addEventListener('readystatechange', (urlPark) => responseMethod(httpRequest));
+    httpRequest.open('GET', url);
+    httpRequest.send();
+};
+createRequest(urlPark);
+
