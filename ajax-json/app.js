d818f00447140e9930d4cd3d9da996fb6a0cd782
Richard A <richard@main-vision.com>
2022-02-01 19:11:58 +0100

    add fetch

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -1,14 +1,4 @@
 'use strict';
-// added for xhr to work: see: https://stackoverflow.com/questions/32604460/xmlhttprequest-module-not-defined-found
-
-// var XMLHttpRequest = require('xhr2');
-// var xhr = new XMLHttpRequest();
-
-//test url
-
-// For Street addresses
-
-//const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&street=1428%20Post%20Aly&street2=&city=Seattle&state=WA&zipcode=98101&candidates=10&match=invalid';
 
 const url = 'https://us-street.api.smartystreets.com/street-address?key=117142354042657436&';
 
@@ -33,8 +23,8 @@ const UrlUpdateUISuccess = function(data) {
     zipField.value = zip + '-' + plus4
 };
 
-const ParkUpdateUISuccess = function(data) {
-    const parsedData = JSON.parse(data);
+const ParkUpdateUISuccess = function(parsedData) {
+//    const parsedData = JSON.parse(data);
     console.log(parsedData);
     const number = Math.floor(Math.random() * parsedData.data.length);
     parkName.textContent = parsedData.data[number].fullName;
@@ -53,22 +43,30 @@ const ParkUpdateUIError = function(error) {
     console.log(error);
 };
 
-const responseMethod = function(httpRequest, succeed, fail) {
-    if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-            succeed(httpRequest.responseText);
-        } else {
-            fail(httpRequest.status + ': ' + httpRequest.responseText);
-        }
-    }
-}
+// const responseMethod = function(httpRequest, succeed, fail) {
+//     if (httpRequest.readyState === 4) {
+//         if (httpRequest.status === 200) {
+//             succeed(httpRequest.responseText);
+//         } else {
+//             fail(httpRequest.status + ': ' + httpRequest.responseText);
+//         }
+//     }
+// }
+
+// const createRequest = function(url, succeed, fail) {
+//     const httpRequest = new XMLHttpRequest(url);
+//     httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail));
+//     httpRequest.open('GET', url);
+//     httpRequest.send();
+// };
+
+// use fetch to make the request
 
 const createRequest = function(url, succeed, fail) {
-    const httpRequest = new XMLHttpRequest(url);
-    httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail));
-    httpRequest.open('GET', url);
-    httpRequest.send();
-};
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => succeed(data));
+}
 
 const checkCompletion = function() {
     if (addressField.value !== '' &&
@@ -83,38 +81,12 @@ const checkCompletion = function() {
 }
 
 //createRequest(url);
-createRequest(urlPark, ParkUpdateUISuccess, ParkUpdateUIError);
+//createRequest(urlPark, ParkUpdateUISuccess, ParkUpdateUIError);
 
 addressField.addEventListener('blur', checkCompletion);
 cityField.addEventListener('blur', checkCompletion);
 stateField.addEventListener('blur', checkCompletion);
+window.addEventListener('DOMContentLoaded', () => {
+    createRequest(urlPark, ParkUpdateUISuccess, ParkUpdateUIError);
+});
 
-/*
-// For National parks
-
-const updateUISuccess = function(data) {
-    console.log(data);
-};
-
-const updateUIError = function(error) {
-    console.log(error);
-};
-
-const responseMethod = function(httpRequest) {
-    if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-            updateUISuccess(httpRequest.responseText);
-        } else {
-            updateUIError(httpRequest.status + ': ' + httpRequest.responseText);
-        }
-    }
-}
-
-const createRequest = function(url) {
-    const httpRequest = new XMLHttpRequest(url);
-    httpRequest.addEventListener('readystatechange', (urlPark) => responseMethod(httpRequest));
-    httpRequest.open('GET', url);
-    httpRequest.send();
-};
-
-*/
