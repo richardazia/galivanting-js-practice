baff24f67c450752fb1bb8f8b0e8d015c5023bf0
Richard A <richard@main-vision.com>
2022-02-04 17:13:22 +0100

    add const title and image

--- instaJson.js
+++ instaJson.js
@@ -6,7 +6,13 @@ fetch('posts_1.json')
         console.log(data);
     });
 
+const title = document.querySelector('#posts h1');
+const image = document.querySelector('#posts img');
+
 const igSuccess = function(data) {
     const parsedData = JSON.parse(data);
     console.log(parsedData);
+    const number = Math.floor(Math.random() * parsedData.data.length);
+    title.textContent = parsedData.data[number].title;
+    image.href = parsedData.data[number].uri;
 };
