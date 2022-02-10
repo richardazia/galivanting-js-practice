943536a234fceaadd31063906112fcf3c58c5a22
Richard A <richard@main-vision.com>
2022-02-10 16:55:04 +0100

    define totalPosts and use while loop

--- instagram/igTimeline.js
+++ instagram/igTimeline.js
@@ -5,11 +5,21 @@ fetch('posts_1.json')
     .then(data => {
         const totalPosts = data.length;
         console.log(totalPosts); // 5257
+
+        var n = totalPosts;
+        var i = 1;
+
+        while (n > 5200) {
+        n--;
+        console.log(n);
+}
+
+
         // generate random number to select a post
-        let postTitle = data[i].media[0].title;
+        let postTitle = data[n].media[0].title;
             if (postTitle == false) {
                 postTitle = 'Untitled';
             }
-            let img = data[i].media[0].uri;
+            let img = data[n].media[0].uri;
             console.log(title + " " + img);
     });
