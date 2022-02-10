9fb96588c848cfac218d2fa8abfdd27cccf4bb47
Richard A <richard@main-vision.com>
2022-02-10 17:03:46 +0100

    remove var i = 1 and add console.logs

--- instagram/igTimeline.js
+++ instagram/igTimeline.js
@@ -7,19 +7,16 @@ fetch('posts_1.json')
         console.log(totalPosts); // 5257
 
         var n = totalPosts;
-        var i = 1;
-
         while (n > 5200) {
         n--;
         console.log(n);
-}
-
-
         // generate random number to select a post
         let postTitle = data[n].media[0].title;
+        console.log(postTitle);
             if (postTitle == false) {
                 postTitle = 'Untitled';
-            }
-            let img = data[n].media[0].uri;
-            console.log(title + " " + img);
+                    }
+        let img = data[n].media[0].uri;
+        console.log(img);
+        }
     });
