7b1129bd0ca013f591f758680ff1dd588041af23
Richard A <richard@main-vision.com>
2022-02-11 17:32:27 +0100

    move code to display individual post within loop

--- instagram/igTimeline.js
+++ instagram/igTimeline.js
@@ -13,43 +13,44 @@ fetch("posts_1.json")
 
     while (number > 5247) {
       number--;
+      console.log("The number inside: " + number);
+      let unix_timestamp = data[number].media[0].creation_timestamp;
+      // convert timestamp to human readable date
+      let date = new Date(unix_timestamp * 1000);
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      console.log(day);
+      console.log(month);
+      console.log(year);
+      // Define post title
+      let postTitle = data[number].media[0].title;
+      // If there is no post title show "Untitled"
+      if (postTitle == false) {
+        postTitle = "Untitled";
+      }
+      // Define post image
+      let img = document.createElement("img");
+      console.log(img);
+      // Define phrase to display
+      let phrase =
+        "I was taken on the " +
+        date +
+        ', showing "' +
+        postTitle +
+        '". I am post No: ' +
+        number +
+        ".";
+      console.log(phrase);
+      document.querySelector("div").appendChild(img);
+      // Find image URI
+      img.src = data[number].media[0].uri;
+      let post = document.getElementById("text");
+      post.innerHTML = phrase;
+      let photo = document.getElementById("photo");
+      photo.appendChild(img);
     }
-    console.log("The number: " + number);
+    console.log("The number outside: " + number);
     // retrieve unix timestamp from post
-    let unix_timestamp = data[number].media[0].creation_timestamp;
-    // convert timestamp to human readable date
-    let date = new Date(unix_timestamp * 1000);
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    // Define post title
-    let postTitle = data[number].media[0].title;
 
-    // If there is no post title show "Untitled"
-    if (postTitle == false) {
-      postTitle = "Untitled";
-    }
-    // Define post image
-    let img = document.createElement("img");
-    console.log(img);
-    // Define phrase to display
-    let phrase =
-      "I was taken on the " +
-      date +
-      ', showing "' +
-      postTitle +
-      '". I am post No: ' +
-      number +
-      ".";
-    console.log(phrase);
-    document.querySelector("div").appendChild(img);
-    // Find image URI
-    img.src = data[number].media[0].uri;
-    let post = document.getElementById("text");
-    post.innerHTML = phrase;
-    let photo = document.getElementById("photo");
-    photo.appendChild(img);
   });
