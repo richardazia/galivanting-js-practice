239bf92ff3c3a04af73911ed8e5b84228f1ffb85
Richard A <richard@main-vision.com>
2022-02-11 17:55:29 +0100

    added comments for what I want to achieve next.

--- instagram/igTimeline.js
+++ instagram/igTimeline.js
@@ -1,5 +1,11 @@
 // I still want to display ten posts, but at least it displays the first post.
 
+// For now all images are displayed in the same div and I want each one to be in its own div.
+// The displays just once, for the last post, and I want it to display for each post. 
+// I want to experiment with let fragment = new DocumentFragment();
+// let div = document.createElement("div");
+// fragment.append(div);
+
 "use strict";
 
 fetch("posts_1.json")
@@ -14,43 +20,43 @@ fetch("posts_1.json")
     while (number > 5247) {
       number--;
       console.log("The number inside: " + number);
-      let unix_timestamp = data[number].media[0].creation_timestamp;
-      // convert timestamp to human readable date
-      let date = new Date(unix_timestamp * 1000);
-      let day = date.getDate();
-      let month = date.getMonth() + 1;
-      let year = date.getFullYear();
-      console.log(day);
-      console.log(month);
-      console.log(year);
-      // Define post title
-      let postTitle = data[number].media[0].title;
-      // If there is no post title show "Untitled"
-      if (postTitle == false) {
-        postTitle = "Untitled";
-      }
-      // Define post image
-      let img = document.createElement("img");
-      console.log(img);
-      // Define phrase to display
-      let phrase =
-        "I was taken on the " +
-        date +
-        ', showing "' +
-        postTitle +
-        '". I am post No: ' +
-        number +
-        ".";
-      console.log(phrase);
-      document.querySelector("div").appendChild(img);
-      // Find image URI
-      img.src = data[number].media[0].uri;
-      let post = document.getElementById("text");
-      post.innerHTML = phrase;
-      let photo = document.getElementById("photo");
-      photo.appendChild(img);
+          let unix_timestamp = data[number].media[0].creation_timestamp;
+    // convert timestamp to human readable date
+    let date = new Date(unix_timestamp * 1000);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    console.log(day);
+    console.log(month);
+    console.log(year);
+    // Define post title
+    let postTitle = data[number].media[0].title;
+    // If there is no post title show "Untitled"
+    // if (postTitle == false) {
+    //   postTitle = "Untitled";
+    // }
+    // Define post image
+    let img = document.createElement("img");
+    console.log(img);
+    // Define phrase to display
+    let phrase =
+      "I was taken on the " +
+      date +
+      ', showing "' +
+      postTitle +
+      '". I am post No: ' +
+      number +
+      ".";
+    console.log(phrase);
+    document.querySelector("div").appendChild(img);
+    // Find image URI
+    img.src = data[number].media[0].uri;
+    let post = document.getElementById("text");
+    post.innerHTML = postTitle;
+    let photo = document.getElementById("photo");
+    photo.appendChild(img);
     }
     console.log("The number outside: " + number);
-    // retrieve unix timestamp from post
+
 
   });
