fd8a0eb3e10088ec7f6c0ad61c2a957529520c17
Richard A <richard@main-vision.com>
2022-02-13 11:19:29 +0100

    remove surplus code and change number to index

--- instagram/igTimeline.js
+++ instagram/igTimeline.js
@@ -15,48 +15,30 @@ fetch("posts_1.json")
     const totalPosts = data.length;
     console.log(totalPosts); // 5257
 
-    var number = totalPosts;
+    var index = totalPosts;
 
-    while (number > 5247) {
-      number--;
-      console.log("The number inside: " + number);
-          let unix_timestamp = data[number].media[0].creation_timestamp;
+    while (index > 5247) {
+      index--;
+      console.log("The number inside: " + index);
+          let unix_timestamp = data[index].media[0].creation_timestamp;
     // convert timestamp to human readable date
     let date = new Date(unix_timestamp * 1000);
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    console.log(day);
-    console.log(month);
-    console.log(year);
     // Define post title
-    let postTitle = data[number].media[0].title;
+    let postTitle = data[index].media[0].title;
     // If there is no post title show "Untitled"
-    // if (postTitle == false) {
-    //   postTitle = "Untitled";
-    // }
+    if (postTitle == false) {
+      postTitle = "Untitled";
+    }
     // Define post image
     let img = document.createElement("img");
     console.log(img);
-    // Define phrase to display
-    let phrase =
-      "I was taken on the " +
-      date +
-      ', showing "' +
-      postTitle +
-      '". I am post No: ' +
-      number +
-      ".";
-    console.log(phrase);
     document.querySelector("div").appendChild(img);
     // Find image URI
-    img.src = data[number].media[0].uri;
+    img.src = data[index].media[0].uri;
     let post = document.getElementById("text");
     post.innerHTML += postTitle;
     let photo = document.getElementById("photo");
     photo.appendChild(img);
     }
-    console.log("The number outside: " + number);
-
-
+    console.log("The number outside: " + index);
   });
