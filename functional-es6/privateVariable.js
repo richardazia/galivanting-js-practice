9e94678e3fe5953d1e632fcc0203f1c056714365
Richard A <richard@main-vision.com>
2022-02-16 18:03:52 +0100

    closure experiment for post

---
+++ functional-es6/privateVariable.js
@@ -0,0 +1,25 @@
+const Post = ({ title, uri, date }) => {
+    var _title = title;
+    var _uri = uri;
+    var _date = date;
+
+    return { 
+        getTitle: () => _title,
+        getUri: () => _uri,
+        getDate: () => _date,
+
+        setTitle: newTitle => _title = newTitle,
+        setDate: newDate => _date = newDate,
+        setUri: newUri => _uri = newUri,
+
+    }
+}
+const post = Post({ title: 'A Cold Rainy Day', uri: 'http://example.com', date: new Date() });
+
+console.log(post.getDate());
+post.setDate('December 17, 1995 03:24:00');
+console.log(post.getDate());
+post.setTitle('A Chicken Running After A Goose');
+console.log(post.getTitle());
+post.setUri('http://example.com/chicken-running-after-goose');
+console.log(post.getUri());
