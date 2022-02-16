1cac381d77f5a34fb2121c6a9a90c0120b735998
Richard A <richard@main-vision.com>
2022-02-16 17:46:56 +0100

    use a closure

---
+++ functional-es6/closure.js
@@ -0,0 +1,11 @@
+// a closure still has access to the outer scope
+
+
+const createTypewriter = () => {
+    const myFavouriteMask = 'FFp2';
+
+    return () => console.log(`I prefer when people wear ${myFavouriteMask} masks around me.`);
+};
+
+const carbonPaper = createTypewriter();
+carbonPaper();
