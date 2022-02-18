1a3f46d1dedd8e15fcaa18af189d024b02dde5d9
Richard A <richard@main-vision.com>
2022-02-18 17:25:57 +0100

    add try and catch for failures

--- json/readJson.js
+++ json/readJson.js
@@ -5,4 +5,11 @@ fs.readFile("play.json", "utf8", (err, jsonString) => {
         return;
         }
         console.log("File data:", jsonString);
-    });
+    // Add try and catch blocks to handle the error if the file is not found.
+    try {
+        const play = JSON.parse(jsonString);
+        console.log(play);
+    } catch (err) {
+        console.log("Error parsing JSON string:", err);
+    }
+});
