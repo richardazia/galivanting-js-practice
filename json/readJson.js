720ed2560d9c615d087ff6968a577bdc10648564
Richard A <richard@main-vision.com>
2022-02-18 17:31:58 +0100

    add mock data to json file

--- json/readJson.js
+++ json/readJson.js
@@ -10,9 +10,11 @@ fs.readFile("play.json", "utf8", (err, jsonString) => {
         console.log("File data:", jsonString);
     // Add try and catch blocks to handle the error if the file is not found.
     try {
+        const jsonString = fs.readFileSync("play.json", "utf8");
         const play = JSON.parse(jsonString);
         console.log(play);
     } catch (err) {
         console.log("Error parsing JSON string:", err);
+        return; 
     }
 });
