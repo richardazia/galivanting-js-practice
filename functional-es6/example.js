272e4556d368ce2c91aaa35154455630395b7668
Richard A <richard@main-vision.com>
2022-02-18 11:14:42 +0100

    change reduce function to be acc * x

--- functional-es6/example.js
+++ functional-es6/example.js
@@ -5,7 +5,7 @@ const numbers = [1, 2, 3, 4, 10, 2, 56, 3, 5, 2, 2, 12, 73, 14, 23, 6, 15];
 const sum = numbers.reduce((acc, x) => {
 console.log(`acc: ${acc}`);
 console.log(`x: ${x}`);
-return acc + x;
-}, 0);
+return acc * x;
+}, 3);
 
 console.log(sum);
