e62fe3d9c799e36ffd15c2691aabed01ff562132
Richard A <richard@main-vision.com>
2022-02-18 11:13:24 +0100

    use reduce function

--- functional-es6/example.js
+++ functional-es6/example.js
@@ -1,23 +1,11 @@
-//Sort through items using a function. 
+// Use reduce 
 
-const mixedUpNumbers = [1, 2, 3, 4, 10, 2, 56, 3, 5, 2, 2, 12, 73, 14, 23, 6, 15];
+const numbers = [1, 2, 3, 4, 10, 2, 56, 3, 5, 2, 2, 12, 73, 14, 23, 6, 15];
 
-const ascending = (a, b) => {
-    if (a < b) return -1; 
-    if (a > b) return 1;
-    return 0;
-}
+const sum = numbers.reduce((acc, x) => {
+console.log(`acc: ${acc}`);
+console.log(`x: ${x}`);
+return acc + x;
+}, 0);
 
-const descending = (a, b) => {
-    if (a > b) return -1; 
-    if (a < b) return 1;
-    return 0;
-}
-
-const sortedNumbers = mixedUpNumbers.slice().sort(ascending);
-
-console.log(sortedNumbers);
-
-const sortedDownNumbers = mixedUpNumbers.slice().sort(descending);
-
-console.log(sortedDownNumbers);
+console.log(sum);
