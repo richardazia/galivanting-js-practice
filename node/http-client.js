1d207d2cf31b4a3ede07ee6c1fbbaa600941a982
Richard A <richard@main-vision.com>
2022-02-21 11:12:23 +0100

    console.error vice console.log.error

--- node/http-client.js
+++ node/http-client.js
@@ -3,5 +3,6 @@ var http = require('http')
 http.get(process.argv[2], function (response) {
     response.setEncoding('utf8')
     response.on('data', console.log)
-    response.on('error', console.log.error)
+    response.on('error', console.error)
 }).on('error', console.error)
+
