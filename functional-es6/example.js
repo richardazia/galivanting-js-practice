84b87b4fedc7342fe94d0c6058e173e78e57b6f1
Richard A <richard@main-vision.com>
2022-02-22 11:31:42 +0100

    add data to example.js

--- functional-es6/example.js
+++ functional-es6/example.js
@@ -1,11 +1,38 @@
-// Use reduce 
+//Data from Learning Functional Programming with Javascript ES6+ Ch3 execise 9
 
-const numbers = [1, 2, 3, 4, 10, 2, 56, 3, 5, 2, 2, 12, 73, 14, 23, 6, 15];
-
-const sum = numbers.reduce((acc, x) => {
-console.log(`acc: ${acc}`);
-console.log(`x: ${x}`);
-return acc * x;
-}, 3);
-
-console.log(sum);
+const employees = [{
+    name: 'John Olsen',
+    age: 54,
+    jobTitle: 'developer',
+    salary: 70000,
+}, {
+    name: 'Karen Norris',
+    age: 34,
+    jobTitle: 'engineer',
+    salary: 75000,
+}, {
+    name: 'Daryl Cline',
+    age: 25,
+    jobTitle: 'secretary',
+    salary: 54000,
+}, {
+    name: 'Abbey Garcia',
+    age: 40,
+    jobTitle: 'developer',
+    salary: 100000,
+}, {
+    name: 'Finn Smith',
+    age: 29,
+    jobTitle: 'engineer',
+    salary: 40000,
+}, {
+    name: 'Eve Wordsworth',
+    age: 20,
+    jobTitle: 'developer',
+    salary: 65000,
+}, {
+    name: 'Ronald Jacobs',
+    age: 60,
+    jobTitle: 'developer',
+    salary: 90000,
+}];
