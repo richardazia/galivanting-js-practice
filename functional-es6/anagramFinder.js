ded1929f817f2ee238b6c6d88fa01f3c6ec1ef30
Richard A <richard@main-vision.com>
2022-02-23 11:53:02 +0100

    replace import with var words and add functionality

---
+++ functional-es6/anagramFinder.js
@@ -0,0 +1,23 @@
+var words = require('an-array-of-english-words')
+
+const countOccurences = arr => {
+    arr.reduce((acc, str) => ({
+        ...acc,
+        [str]: acc[str] ? acc[str] + 1 : 1,
+    }), {});
+
+const hasSameLetterCount = (word1, word2) => {
+    const word1Count = countOccurences(word1.split(''));
+    const word2Count = countOccurences(word2.split(''));
+
+    return Object.keys(word1Count).length === Object.keys(word2Count).length
+        && Object.keys(word1Count).every(letter => word1Count[letter] === word2Count[letter]);
+}
+
+const findAnagrams = (word, allWords) => {
+    return allWords
+        .filter(entry => hasSameLetterCount(word, entry))
+        .filter(anagram => anagram !== word);
+};
+
+console.log(findAnagrams('cinema', words)); // ['iceman', 'manic', 'incima']
