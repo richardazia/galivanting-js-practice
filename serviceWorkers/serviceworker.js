7efe8ca4cc99d6b00d3eed7ebbe3115d0ef5210a
Richard A <richard@main-vision.com>
2022-02-25 17:35:01 +0100

    and error message to serviceworker.js

--- serviceWorkers/serviceworker.js
+++ serviceWorkers/serviceworker.js
@@ -4,7 +4,7 @@ console.log("We are a service worker");
 try {
     importScripts("events.js");
 } catch (e) {
-    
+    console.log("Error importing events.js");
 }
 
 console.log("after loading the events.js script");
