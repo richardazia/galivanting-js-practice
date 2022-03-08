13149527820e931c581bf16a5df4035868e4f423
Richard A <richard@main-vision.com>
2022-03-08 17:40:37 +0100

    no change from course page.

---
+++ domPractice/queryAll.js
@@ -0,0 +1,40 @@
+// Code from: https://github.com/LinkedInLearning/vanilla-js-DOM-2876283/blob/main/Chapter1/01_03/queryall.js
+/* eslint no-undef: "error" */
+/* eslint-env browser */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.querySelector('textarea[name=source]');
+    textarea.addEventListener('input', (e) => {
+        refreshOutput(e.target.value);
+    });
+
+    document.forms.queryall.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        refreshQuery(e.target.selector.value);
+
+        return false;
+    });
+
+    refreshOutput(textarea.value);
+});
+
+function refreshOutput(html) {
+    const output = document.querySelector('#output');
+
+    output.innerHTML = html;
+}
+
+function refreshQuery(selector) {
+    const output = document.querySelector('#output');
+    const matches = document.querySelector('#matches');
+
+    matches.innerHTML = '';
+
+    output.querySelectorAll(selector).forEach((el) => {
+        const li = document.createElement('li');
+        li.innerText = el.outerHTML;
+
+        matches.appendChild(li);
+    });
+}
