4f71be4c25f8b22dd7a0121af6f1bae5e00686af
Richard A <richard@main-vision.com>
2022-03-15 17:46:39 +0100

    challenge 1

---
+++ jsc2k22/challenges.js
@@ -0,0 +1,5 @@
+var country = "Switzerland";
+var continent = "Europe";
+var population = 8000000;
+
+console.log (`${country} is in the middle of ${continent} with a population of ${population} people.`);
