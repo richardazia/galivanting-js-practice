9c27a7ea5f4ca6693e2a1904708eba24c111f9ff
Richard A <richard@main-vision.com>
2022-03-20 12:11:05 +0100

    bmi challenge

--- misc/play.js
+++ misc/play.js
@@ -17,18 +17,48 @@ const ageJohn = thisYear - 1990;
 
 //Operator Precedence - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
 
-const now = 2022;
+// const now = 2022;
 
-console.log(now - 1984 > now - 1990);
+// console.log(now - 1984 > now - 1990);
 
-console.log(20 - 10 - 5);
+// console.log(20 - 10 - 5);
 
-let x, y;
+// let x, y;
 
-x = y = 25 - 10 -5; // it will go from left to right for the maths part, and then from right to left for the assignment part
+// x = y = 25 - 10 -5; // it will go from left to right for the maths part, and then from right to left for the assignment part
 
-console.log(x, y);
+// console.log(x, y);
 
-const averageAge = (ageBob + ageJohn) / 2; // add the two ages and divide the result by two. We tell it how to calculate, instead of letting it assume.
+// const averageAge = (ageBob + ageJohn) / 2; // add the two ages and divide the result by two. We tell it how to calculate, instead of letting it assume.
 
-console.log(ageBob, ageJohn, averageAge);
+// console.log(ageBob, ageJohn, averageAge);
+
+console.log("The challenge");
+
+markHeight = 1.69;
+markMass = 78;
+markHeightTall = 1.88;
+markMassTall = 95;
+
+JohnHeight = 1.95;
+JohnMass = 92;
+JohnSmallHeight = 1.76;
+JohnSmallMass = 85;
+
+markBmi = markMass / markHeight ** 2; 
+johnBMI = JohnMass / (JohnHeight * JohnHeight);
+
+markTallBMI = markMassTall / markHeightTall ** 2;
+johnTallBMI = JohnMass / (JohnSmallHeight * JohnSmallHeight);
+
+markTallHigherBMI = markTallBMI > markBmi;
+
+console.log(markBmi, johnBMI);
+
+console.log(markTallBMI, johnTallBMI);
+
+markHigherBMI = markBmi > johnBMI;
+
+console.log(markHigherBMI);
+
+console.log(markTallHigherBMI);
