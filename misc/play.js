7f41e3008909f28e4e88c7fd2fa88df26bd8d04c
Richard A <richard@main-vision.com>
2022-03-20 11:24:15 +0100

    experiment with a few operators

---
+++ misc/play.js
@@ -0,0 +1,16 @@
+const thisYear = new Date().getFullYear();
+
+const ageBob = thisYear - 1984;
+const ageJohn = thisYear - 1990;
+
+console.log("Bob is " + ageBob + " and John is " + ageJohn);
+
+// play with maths functions in javascript, ** is exponential ("2 * 2 * 2")
+
+console.log(ageBob * 2, ageJohn * 2);
+console.log(ageBob * ageJohn, 2 ** 3);
+
+firstName = "Donald";
+lastName = "Duck";
+
+console.log(firstName + " " + lastName);
