37061e29ba3a68df3ad4e672f68fb7a180e12c94
Richard A <richard@main-vision.com>
2022-03-25 12:13:36 +0100

    attempt to refactor randomigpost draft

--- instagram/randomRefactored.js
+++ instagram/randomRefactored.js
@@ -3,6 +3,7 @@
 fetch('posts_1.json') 
     .then(response => response.json())
     .then(data => {
+        const dataLength = data.length;
         const number = Math.floor(Math.random() * data.length); 
         let unix_timestamp = data[number].media[0].creation_timestamp;
         let date = new Date(unix_timestamp * 1000);
@@ -11,13 +12,18 @@ fetch('posts_1.json')
         if (postTitle == false) {
             postTitle = 'Rename me!';
         }
-        let phrase = `I was taken on ${day}, showing "${postTitle}". I am post No: ${number}.`;
+        let phrase = 
+            "I was taken on the " 
+            + day + ", showing \"" 
+            + postTitle + "\". I am post No: " 
+            + number + ".";
         let img = document.createElement('img');
-        document.querySelector('div')
+    document.querySelector('div')
         .appendChild(img);
-        img.src = data[number].media[0].uri; 
-        let post = document.getElementById('text');
-            post.innerHTML = phrase;
-        let photo = document.getElementById('photo');
-            photo.appendChild(img);
+    img.src = data[number].media[0].uri; 
+    let post = document.getElementById('text');
+        post.innerHTML = phrase;
+    let photo = document.getElementById('photo');
+        photo.appendChild(img);
 });
+
