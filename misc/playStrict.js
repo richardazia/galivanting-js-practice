008ae7270f6871ced06aea76951ba32092f15753
Richard A <richard@main-vision.com>
2022-03-27 11:15:35 +0200

    challenge attempt

--- misc/playStrict.js
+++ misc/playStrict.js
@@ -136,7 +136,7 @@
 //     return average;
 // }
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 // 2. Use the function to calculate the average for both teams
 
@@ -144,40 +144,70 @@ const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 // const avgDolphins = calcAverage(44, 23, 71);
 // const avgKoalas = calcAverage(65, 54, 49);
 // Data 2
-const avgDolphins = calcAverage(85, 54, 41);
-const avgKoalas = calcAverage(23, 34, 27);
+// const avgDolphins = calcAverage(85, 54, 41);
+// const avgKoalas = calcAverage(23, 34, 27);
 
-// 3. Create a function 'checkWinner' that takes the average score of each team 
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner 
-// to the console, together with the victory points, according to the rule above. 
-// Example: "Koalas win (30 vs. 13)"
+// // 3. Create a function 'checkWinner' that takes the average score of each team 
+// // as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner 
+// // to the console, together with the victory points, according to the rule above. 
+// // Example: "Koalas win (30 vs. 13)"
 
-function checkWinner (avgDolphins, avgKoalas) {
-    if (avgDolphins > avgKoalas * 2) {
-        console.log("Dolphins win (", avgDolphins, " vs. ", avgKoalas, ")");
-    } else if (avgKoalas > avgDolphins * 2) {
-        console.log("Koalas win (", avgKoalas, " vs. ", avgDolphins, ")");
+// function checkWinner (avgDolphins, avgKoalas) {
+//     if (avgDolphins > avgKoalas * 2) {
+//         console.log("Dolphins win (", avgDolphins, " vs. ", avgKoalas, ")");
+//     } else if (avgKoalas > avgDolphins * 2) {
+//         console.log("Koalas win (", avgKoalas, " vs. ", avgDolphins, ")");
+//     } else {
+//         console.log("The teams had a Draw (", avgDolphins, " vs. ", avgKoalas, ")");
+//     }
+// }
+
+// // 4. Use the 'checkWinner' function to determine the winner for both Data 1 and 
+// // Data 2
+// console.log(checkWinner(avgDolphins, avgKoalas));
+
+
+/*
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns 
+the corresponding tip, calculated based on the rules above (you can check out 
+the code from first tip calculator challenge if you need to). Use the function 
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data 
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from 
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+*/
+
+function calcTip (bills) {
+    if (bills < 50) {
+        return bills * 0.15;
+    } else if (bills > 300) {
+        return bills * 0.20;
     } else {
-        console.log("The teams had a Draw (", avgDolphins, " vs. ", avgKoalas, ")");
+        return bills * 0.15;
     }
 }
 
-// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and 
-// Data 2
-console.log(checkWinner(avgDolphins, avgKoalas));
+function iterator (bills) {
+    const tips = [];
+    for (let i = 0; i < bills.length; i++) {
+        tips.push(calcTip(bills[i]));
+    }
+    const total = [];
+    for (let i = 0; i < bills.length; i++) {
+        total.push(bills[i] + tips[i]);
+    }
+    return [tips, total];
+}
 
-
-// 5. Ignore draws this time
-// Test data:
-
-// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-
-// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-
-// Hints:
-
-// § To calculate average of 3 values, add them all together and divide by 3
-
-// § To check if number A is at least double number B, check for A >= 2 * B. 
-
-// Apply this to the team's average scores �
+const bills = [125, 555, 44];
+const tips = iterator(bills);
+console.log(bills);
+console.log(tips);
