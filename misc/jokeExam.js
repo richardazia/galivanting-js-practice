595068b52d640d4bd1c26bcd8ed4643b313e676c
Richard A <richard@main-vision.com>
2022-04-25 12:09:11 +0200

    May play with this later

---
+++ misc/jokeExam.js
@@ -0,0 +1,27 @@
+/*
+
+Would you have passed the exam to become a Foreign Office clerk in 1876? 
+
+Here’s the Constitutional History of England paper. 
+
+1. What were the chief administrative and judicial changes introduced by William the Conqueror? 
+
+2. What abuses of the prerogatives of the crown furnished the chief grounds of compliant against the government of Henry II and his immediate successors?
+
+3. Give a History of the House of Commons in the reigns of Edward III and Richard II
+
+4. What questions have at different times arisen as to the construction of the leading provisions of the Statute of Treasons?
+
+5. Give a History of the Star Chamber.
+
+6. What at different periods has been the force of Royal Proclamations?
+
+7. What were the chief incidents in constitutional history during the period 1629-40 in which Charles I governed without a Parliament?
+
+8. What violations of the constitution on the part of Charles II led to the revolution of 1688?
+
+9. When and on what grounds were General Warrants declared illegal?
+
+10. What were the chief questions of constitutional law that arose during Pitt’s Ministry (1783-1801)
+
+*/
