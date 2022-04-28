ef58dfe04e1f8c19b1ccb0b698aa6c89cdc311cd
Richard A <richard@main-vision.com>
2022-04-28 12:22:22 +0200

    minor changes

--- misc/scopePractice.js
+++ misc/scopePractice.js
@@ -1246,14 +1246,47 @@ console.log(maskVisa(120027042020));
 */
 // Coding Challenge #4
 
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("btn"));
+
 const variableNames = [
 	"underscore_case",
-	"underscore_case",
 	"first_name",
-	"Some_Variable",
-	"calculate_AGE",
-	"delayed_departure",
+	"   Some_Variable ",
+	" calculate_AGE",
+	"   delayed_departure",
 ];
+document.getElementsByTagName("textarea")[0].value = variableNames.join("\n");
+// First version
 
 variableNames.forEach((variableName) => {
 	const splitWords = variableName.split("_");
@@ -1263,11 +1296,6 @@ variableNames.forEach((variableName) => {
 		capitaliseWords.push(word[0].toUpperCase() + word.slice(1));
 	}
 	const joinedWords =
-		capitaliseWords[0].toLowerCase() + capitaliseWords.slice(1).join("");
+		capitaliseWords[0].toLowerCase() + capitaliseWords.slice(1).join("") + "✅";
 	console.log(joinedWords);
-	document.body.append(document.createElement("textarea"));
-	document.body.append(document.createElement("btn"));
 });
-
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("btn"));
