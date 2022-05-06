214d94f64597621802c9ebb2658bf6da1beaaea3
Richard A <richard@main-vision.com>
2022-05-06 17:27:32 +0200

    challenge attempt  add a poll

--- misc/functionPractice.js
+++ misc/functionPractice.js
@@ -282,3 +282,155 @@ const addVAT2 = addTaxRate(0.23);
 
 console.log(addVAT2(100));
 console.log(addVAT2(10));
+
+// Challenge 1: Add a poll
+
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+	// Create the object with the question, and within the object create the array of options.
+	question: "What is your favourite programming language?",
+	options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+	// It now displays as an object with two sections: question and options. The Array is numbered from 0 onwards with a length of 4.
+	// The slow option
+	//answers: [0, 0, 0, 0],
+	// The fast option
+	answers: new Array(4).fill(0), // We specify that we want an array of length 4, filled with zeroes.
+	registerNewAnswer() {
+		const answer = Number(
+			prompt(
+				`${this.question}\n${this.options.join("\n")}\n(Write option number)`
+			)
+		);
+		console.log(answer);
+
+		typeof answer === "number" &&
+			answer < this.answers.length &&
+			this.answers[answer]++;
+
+		this.displayResults();
+		this.displayResults("string");
+	},
+
+	displayResults(type = "array") {
+		if (type === "array") {
+			console.log(this.answers);
+		} else if (type === "string") {
+			console.log(`Poll results are ${this.answers.join(", ")}`);
+		}
+	},
+};
+console.log(poll);
+// document.querySelector(".poll").addEventListener("click", function (event) {
+// 	console.log("poll clicked");
+document
+	.querySelector(".poll")
+	.addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+const survey = {
+	question: "Do you want to go right or left?",
+	options: ["left", "right"],
+	directions: new Array(2).fill(0),
+	registerNewDirection() {
+		const direction = Number(
+			prompt(
+				`${this.question}\n${this.options[0]}\n${this.options[1]}\n(Write option number)`
+			)
+		);
+		console.log(direction);
+
+		typeof direction === "number" &&
+			direction < this.directions.length &&
+			this.directions[direction]++;
+
+		this.displayChoices();
+		this.displayChoices("string");
+	},
+
+	displayChoices(type = "array") {
+		if (type === "array") {
+			console.log(this.directions);
+		} else if (type === "string") {
+			console.log(
+				`Poll results are ${this.directions[0]}, ${this.directions[1]}`
+			);
+		}
+	},
+};
+
+console.log(survey);
+
+document
+	.querySelector(".survey")
+	.addEventListener("click", survey.registerNewDirection.bind(survey));
+
+const watches = {
+	question: "Which is your favourite watch brand?",
+	options: ["Apple", "Samsung", "Suunto", "Garmin", "Casio", "Timex"],
+
+	answers: new Array(6).fill(0),
+	registerNewAnswer() {
+		const answer = Number(
+			prompt(
+				`${this.question}\n${this.options.join("\n")}\n(Write option number)`
+			)
+		);
+		console.log(answer);
+
+		typeof answer === "number" &&
+			answer < this.answers.length &&
+			this.answers[answer]++;
+
+		this.displayResults();
+		this.displayResults("string");
+	},
+
+	displayResults(type = "array") {
+		if (type === "array") {
+			console.log(this.answers);
+		} else if (type === "string") {
+			console.log(`Poll results are ${this.answers.join(", ")}`);
+		}
+	},
+};
+
+console.log(watches);
+
+document
+	.querySelector(".watches")
+	.addEventListener("click", watches.registerNewAnswer.bind(watches));
+
+watches.displayResults.call({ answers: [5, 2, 3] }, "string");
+watches.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+watches.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
