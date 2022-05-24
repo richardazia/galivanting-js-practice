72b90a8dc8e19be2e42e7026e49b538528190dfe
Richard Azia <richard@main-vision.com>
2022-05-24 18:00:16 +0200

    lesson 196 and 197 - 197 to be completed

--- misc/13-Advanced-DOM-Bankist/starter/script.js
+++ misc/13-Advanced-DOM-Bankist/starter/script.js
@@ -9,16 +9,19 @@ const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
 ///////////////////////////////////////
 // Modal window
 
-const openModal = function (e) {
+const openModal = function(e) {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -28,7 +31,7 @@ btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function(e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
@@ -37,7 +40,7 @@ document.addEventListener('keydown', function (e) {
 /////////////////////////////////////////////////
 //Scrolling
 
-btnScrollTo.addEventListener('click', function (e) {
+btnScrollTo.addEventListener('click', function(e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
@@ -80,7 +83,7 @@ btnScrollTo.addEventListener('click', function (e) {
 // });
 
 // 1. Add event listener to common parent element.
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+document.querySelector('.nav__links').addEventListener('click', function(e) {
   e.preventDefault();
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
@@ -100,7 +103,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // Use event delegation instead
 
-tabsContainer.addEventListener('click', function (e) {
+tabsContainer.addEventListener('click', function(e) {
   const clicked = e.target.closest('.operations__tab');
   // const clicked = e.target; // If we click on the numbers we get the span element
   // const clicked = e.target.parentElement; // also does not work as it selects the parent element one layer too highligh
@@ -121,15 +124,74 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 
+// Menu highlight Navigation elements and fade others
+const navHover = function(e) {
+  console.log(this, e.currentTarget);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', navHover.bind(0.5));
+nav.addEventListener('mouseout', navHover.bind(1));
+
+// Sticky Navigation
+/*
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+*/
+// Option 2
+// nav.addEventListener('mouseover', function(e) {
+//   navHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function(e) {
+//   navHover(e, 1);
+// });
+
+//Sticky Navigation: Intersection Observer API
+const obsCallBack = function(entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2]
+};
+
+const observer = new IntersectionObserver(obsCallBack, obsOptions);
+observer.observe(section1);
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  console.log(entry);
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0
+});
+headerObserver.observe(header);
+
 /////////////////////////////////////////////////
 
 // Lesson 186
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
 document.getElementById('section--1');
@@ -158,7 +220,7 @@ header.after(message);
 // Deleting Elements
 document
   .querySelector('.btn-btn--close-cookie')
-  .addEventListener('click', function () {
+  .addEventListener('click', function() {
     // message.remove();
     message.parentElement.removeChild(message);
   });
@@ -168,36 +230,36 @@ document
 //lesson 187
 //styles
 
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-console.log(message.style.height); // only works for styles we set ourselves in line.
-console.log((message.style.backgroundColor = '#37383d'));
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'; // [Violation] 'setTimeout' handler took 147ms
-
-document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-logo.alt = 'Stupendous, stunning, minimalist logo';
-
-// Non Standard fields
-console.log(logo.designer); // will not work
-console.log(logo.getAttribute('designer')); // will work
-
-logo.setAttribute('company', 'Bankist');
-
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// console.log(message.style.height); // only works for styles we set ourselves in line.
+// console.log((message.style.backgroundColor = '#37383d'));
+//
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+//
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'; // [Violation] 'setTimeout' handler took 147ms
+//
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+//
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+// logo.alt = 'Stupendous, stunning, minimalist logo';
+//
+// // Non Standard fields
+// console.log(logo.designer); // will not work
+// console.log(logo.getAttribute('designer')); // will work
+//
+// logo.setAttribute('company', 'Bankist');
+//
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
 // Data Attributes
 
@@ -211,25 +273,26 @@ logo.classList.contains('ogl');
 
 /////////////////////////////////////////////////
 // Events and Event Handlers
-const h1 = document.querySelector('h1');
-h1.addEventListener('mouseenter', function (e) {
-  console.log('addEventListener: This is the header');
-});
-h1.onmouseenter = function (e) {
-  console.log('I am the old method: onmouseenter');
-};
-
-const logH1 = function (e) {
-  console.log("Ceçi n'est pas un H1");
-
-  h1.removeEventListener('mouseenter', logH1);
-};
-
-h1.addEventListener('mouseenter', logH1);
-
-setTimeout(() => h1.removeEventListener('mouseenter', logH1), 3000);
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', function(e) {
+//   console.log('addEventListener: This is the header');
+// });
+// h1.onmouseenter = function(e) {
+//   console.log('I am the old method: onmouseenter');
+// };
+//
+// const logH1 = function(e) {
+//   console.log("Ceçi n'est pas un H1");
+//
+//   h1.removeEventListener('mouseenter', logH1);
+// };
+//
+// h1.addEventListener('mouseenter', logH1);
+//
+// setTimeout(() => h1.removeEventListener('mouseenter', logH1), 3000);
 
 //Bubbling and capturing
+/*
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -237,7 +300,7 @@ const randomColor = () =>
   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 console.log(randomColor(0, 255));
 
-document.querySelector('.nav__link').addEventListener('click', function (e) {
+document.querySelector('.nav__link').addEventListener('click', function(e) {
   this.style.backgroundColor = randomColor();
   console.log('link', e.target, e.currentTarget);
   console.log(e.currentTarget === this);
@@ -246,7 +309,7 @@ document.querySelector('.nav__link').addEventListener('click', function (e) {
   // e.stopPropagation();
 });
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+document.querySelector('.nav__links').addEventListener('click', function(e) {
   this.style.backgroundColor = randomColor();
   console.log('container', e.target, e.currentTarget);
   console.log(e.currentTarget === this);
@@ -254,13 +317,14 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 document.querySelector('.nav').addEventListener(
   'click',
-  function (e) {
+  function(e) {
     this.style.backgroundColor = randomColor();
     console.log('navbar', e.target, e.currentTarget);
     console.log(e.currentTarget === this);
   },
   true
 );
+*/
 /*
 Capturing
 When an event occurs it is captured it descends from the document to the anchor or other element.
@@ -274,34 +338,34 @@ As in scuba diving when an event has taken place, for example a click on a hyper
 
 // DOM traversing
 // Downwards
-console.log(h1.querySelector('.highlight'));
-
-console.log(h1.childNodes);
-console.log(h1.children);
-console.log((h1.firstElementChild.style.color = 'white'));
-console.log((h1.lastElementChild.style.color = 'orange'));
-
-//Going up
-console.log(h1.parentNode);
-console.log(h1.parentElement);
-
-h1.closest('.header').style.background = 'var(--gradient-secondary)';
-
-h1.closest('h1').style.background = 'var(--gradient-primary)';
-
-// Sibling selection - Sideways
-
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
-
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
-
-console.log(h1.parentElement.children);
-
-[...h1.parentElement.children].forEach(function (el) {
-  if (el !== h1) el.style.transform = 'scale(0.5)';
-});
+// console.log(h1.querySelector('.highlight'));
+//
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// console.log((h1.firstElementChild.style.color = 'white'));
+// console.log((h1.lastElementChild.style.color = 'orange'));
+//
+// //Going up
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+//
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+//
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+//
+// // Sibling selection - Sideways
+//
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+//
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+//
+// console.log(h1.parentElement.children);
+//
+// [...h1.parentElement.children].forEach(function(el) {
+//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// });
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
