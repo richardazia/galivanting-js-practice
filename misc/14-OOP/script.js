28090f6095b591fe9349ea0aaa5130a2e019fe2d
Richard Azia <richard@main-vision.com>
2022-05-31 10:57:16 +0200

    challenge 2 attempt

--- misc/14-OOP/script.js
+++ misc/14-OOP/script.js
@@ -243,3 +243,51 @@ const farah = Object.create(PersonProto);
 farah.init("Farah", 1982);
 farah.calcAge();
 ////////////////////////////////////////
+// Coding Challenge 2
+/*
+Coding Challenge #2
+Your tasks:
+1. Re-create Challenge#1,but this time using an ES6 class (call it 'CarCl')
+2. Add a getter called 'speedUS' which returns the current speed in mi/h(divide
+by 1.6)
+3. Add a setter called'speedUS'which sets the current speed in mi/h (but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+4. Create a new car and experiment with the'accelerate' and 'brake'
+methods, and with the getter and setter.
+Test data:
+§ Data car 1: 'Ford' going at 120 km/h GOOD LUCK 😀
+
+*/
+console.log("Challenge #2");
+
+// #1
+class CarCL {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed / 1.6;
+  }
+
+  calcMph() {
+    console.log(this.speed * 1.6);
+  }
+
+  // #2
+  get speedUS() {
+    return this.speed;
+  }
+
+  get accelerate() {
+    this.speed;
+  }
+  // #3
+  set speedUS(miles) {
+    this.speed.push(miles);
+  }
+
+
+const ford = new CarCL("Ford", 120);
+console.log(ford);
+ford.calcMph();
+console.log(ford.accelerate);
+
+// #4
