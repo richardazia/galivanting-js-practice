368234c1b89aec86ef563e845bc88e46f4fe5aac
Richard Azia <richard@main-vision.com>
2022-06-06 12:04:37 +0200

    use restcountry api

--- misc/16-Asynchronous/script.js
+++ misc/16-Asynchronous/script.js
@@ -4,3 +4,42 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+// https://restcountries.com/v2
+
+const getCountryData = function(country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function() {
+    console.log(this.responseText);
+    // Destructure data with [data]
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    console.log(data.flag);
+    console.log(data.languages);
+    console.log(data.currencies);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>
+      ${(+data.population / 1000000).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData("poland");
+getCountryData("switzerland");
+getCountryData("italy");
+getCountryData("gb");
