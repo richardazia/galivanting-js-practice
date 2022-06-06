357966488088d24153bdd40fc40d74a13dd75c13
Richard Azia <richard@main-vision.com>
2022-06-06 16:55:48 +0200

    test with version 2 of api

--- misc/16-Asynchronous/script.js
+++ misc/16-Asynchronous/script.js
@@ -4,7 +4,8 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-// https://restcountries.com/v2
+
+// https://restcountries.com/v3.1
 
 const getCountryData = function(country) {
   const request = new XMLHttpRequest();
@@ -12,13 +13,10 @@ const getCountryData = function(country) {
   request.send();
 
   request.addEventListener("load", function() {
-    console.log(this.responseText);
     // Destructure data with [data]
     const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    console.log(data.flag);
-    console.log(data.languages);
     console.log(data.currencies);
+    console.log(data.languages);
 
     const html = `
   <article class="country">
@@ -27,9 +25,9 @@ const getCountryData = function(country) {
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>
-      ${(+data.population / 1000000).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+      ${(+data.population / 1000000).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
     </div>
   </article>
   `;
@@ -39,6 +37,38 @@ const getCountryData = function(country) {
   });
 };
 
+/*
+// Using V2
+
+const getCountryData = function(country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function() {
+    // Destructure data with [data]
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>
+      ${(+data.population / 1000000).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+*/
 getCountryData("poland");
 getCountryData("switzerland");
 getCountryData("italy");
