103460f9f85125fd45b3a428acf07dffd4b7d5d3
Richard Azia <richard@main-vision.com>
2022-06-07 16:27:57 +0200

    Use fetch to get data

--- misc/16-Asynchronous/script.js
+++ misc/16-Asynchronous/script.js
@@ -54,11 +54,12 @@ const renderCountry = function(data, className = "") {
 </div>
 </article>
 `;
+  console.log(data.languages[0]);
 
   countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
 };
-
+/*
 const getCountryAndNeighbour = function(country) {
   // Country 1 request
   const request = new XMLHttpRequest();
@@ -95,7 +96,7 @@ const getCountryAndNeighbour = function(country) {
 };
 
 getCountryAndNeighbour("uk");
-
+/*
 /////////////////////////////////////////////////
 // Callback hell example
 
@@ -141,3 +142,25 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+*/
+// const request = fetch("https://restcountries.com/v2/name/italy");
+
+// const getCountryData = function(country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function(response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+// Simplified
+const getCountryData = function(country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData("jamaica");
