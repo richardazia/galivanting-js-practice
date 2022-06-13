c4aa6d3901197cf8b4685301ca8095170681592e
Richard Azia <richard@main-vision.com>
2022-06-13 10:32:02 +0200

    create file and add tasks

---
+++ misc/16-Asynchronous/createImage.js
@@ -0,0 +1,18 @@
+'use strict'
+
+To do: 
+
+// 1. Create a function CreateImage which receives imgPath as an input. 
+
+//2. Return a promise that returns a new image (document.createElement('img') and set the .src atttribute to the provided image path.
+
+// 3. Append it to the DOM with the images class. 
+
+// Part 2
+// Add a .then error handler
+
+// Images load, display for 2 seconds, and then dissapear again.
+
+ 
+
+
