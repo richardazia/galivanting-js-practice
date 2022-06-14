2693105233b10ba6dbaab244724e2c43f2d158fb
Richard Azia <richard@main-vision.com>
2022-06-14 11:13:32 +0200

    up to sort challenge attempt

--- misc/javascriptArrays/script.js
+++ misc/javascriptArrays/script.js
@@ -43,16 +43,56 @@ console.log(animals.sort());
 
 
 const grades = [77, 88, 93, 23, 65, 62, 56, 34, 90, 12, 100];
+/*
 // Highest to loweest
 grades.sort((a,b) => b - a );
 console.log(grades);
 // Lowest to highest
+
 grades.sort((a, b) => a - b);
 console.log(grades);
-
+*/
 const participants = ['Anna', 'Julian', 'John', 'Duckberg', 'leppy the Leprachaun'];
 
 const reversed = [...participants]; 
 
 console.log('reversed', reversed.reverse());
 console.log('untouched original array: ', participants); 
+// Find the first under eighty
+const underEighty = grades.find((e) => e < 80);
+console.log(underEighty);
+// Find the index of the first grade that is over eighty
+const overEighty = grades.findIndex((e) => e > 80);
+console.log(overEighty);
+console.log(grades[1]); 
+
+// Challenge: Order Data
+
+const students = [
+	{name: 'John', grade: 75 },
+	{name: 'Jane', grade: 90 },
+	{name: 'Samantha', grade: 90 },
+	{name: 'Michael', grade: 94 },
+];
+
+console.log(students);
+
+// 2.  Sort student array based on grades (descending order)
+students.sort(function (a,b) {
+	return a.grade - b.grade;
+});
+console.log(students);
+
+// 1. Reverse the order of the array
+students.sort(function(a,b) {
+	return b.grade - a.grade;
+});
+console.log(students);
+
+// 3. Find a student in the array who has a grade over 90
+
+grade = [students.grade[0]];
+console.log(grade);
+const overNinety = students.find((e) => e < 90);
+console.log(overNinety); 
+ 
