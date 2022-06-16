f7c83e9ffa2d440dc750710bb665af5e2649eeb4
Richard Azia <richard@main-vision.com>
2022-06-16 10:49:13 +0200

    course completed

--- misc/jsAlgorithms/script.js
+++ misc/jsAlgorithms/script.js
@@ -1,4 +1,4 @@
-'use strict'
+// 'use strict'
 
 function addUpTo(n) {
 	let total = 0;
@@ -8,4 +8,23 @@ function addUpTo(n) {
 	return total;
 }
 
+// Timer function
+var t1 = performance.now();
+addUpTo(1000000000000); //should be a billion
+
+var t2 = performance.now();
+console.log(`Time elapsed: ${( t2 - t1 ) / 1000} seconds.`)
+
+console.log(addUpTo(8));
+
+/*
+function addUpTo(n) {
+	return n * (n + 1) /2;
+}
+
 console.log(addUpTo(6));
+*/
+
+// ToDo: Look up performance.now later
+
+
