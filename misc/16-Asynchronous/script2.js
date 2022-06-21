542edbb678d58603fad07cf04a6de905db44bdc9
Richard Azia <richard@main-vision.com>
2022-06-21 10:50:01 +0200

    copied challenge code with a few minor typos

--- misc/16-Asynchronous/script2.js
+++ misc/16-Asynchronous/script2.js
@@ -159,11 +159,64 @@ Promise.any([
   Promise.resolve('success'),
   Promise.reject('HB-JNP'),
   Promise.resolve('success'),
-]).then(res => console.log(`any result: ${res}`));
+]).then(res => console.log(`res}`));
 
+//////////
+// The copy 
 
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
+const imgContainer = document.querySelector('.images');
 
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+const LoadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2)
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 Loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(['img/img-1.jpeg', 'img/img-2.jpeg', 'img/img-3.jpeg']);
 
 
 
