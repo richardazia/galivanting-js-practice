9c2f07bf9b4abcce58246658dc568b43d21ba8de
Richard Azia <richard@main-vision.com>
2022-06-27 11:34:11 +0200

    pagination part 1

--- misc/18-forkify/src/js/model.js
+++ misc/18-forkify/src/js/model.js
@@ -1,12 +1,14 @@
 import { async } from "regenerator-runtime";
-import { API_URL } from "./config.js";
+import { API_URL, RES_PER_PAGE } from "./config.js";
 import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
   search: {
     query: '',
-    results: [], 
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -51,4 +53,12 @@ export const loadSearchResults = async function (query) {
   } catch (err) {
     console.error(`${err} query failed, please try again`)
   }
+};
+
+export const getSearchResultsPage = function(page = state.search.page) {
+  state.search.page = page;
+  const start = (page -1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9
+
+  return state.search.results.slice(start, end);
 };
