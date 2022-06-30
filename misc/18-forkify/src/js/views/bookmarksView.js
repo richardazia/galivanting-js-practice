528efbb23509a174950e2352a40cbb69977f436d
Richard Azia <richard@main-vision.com>
2022-06-30 18:21:06 +0200

    previewview and bookmarkview added

---
+++ misc/18-forkify/src/js/views/bookmarksView.js
@@ -0,0 +1,36 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class BookmarksView extends View {
+	_parentElement = document.querySelector('.bookmarks__list');
+	_errorMessage = 'No bookmarks saved. Start collecting recipes.';
+	_message = '';
+
+	_generateMarkup() {
+		console.log(this._data);
+		return this._data.map(this._generateMarupPreview).join('');
+	}
+
+	_generateMarkup() {
+		const id = window.location.hash.slice(1);
+
+		return `
+          <li class="preview">
+            <a class="preview__link ${
+            	result.id === id ? 'preview__link--active' : ''
+          }" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="${result.title}" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+                </div>
+              </div>
+            </a>
+          </li>
+		`;
+	}
+}
+
+export default new BookmarksView();
