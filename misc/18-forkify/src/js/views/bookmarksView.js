09fe24d192028bd0c7a35ae1ebfc5cc9e8570204
Richard Azia <richard@main-vision.com>
2022-07-01 11:09:20 +0200

    import preview view

--- misc/18-forkify/src/js/views/bookmarksView.js
+++ misc/18-forkify/src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class BookmarksView extends View {
@@ -6,30 +7,14 @@ class BookmarksView extends View {
 	_errorMessage = 'No bookmarks saved. Start collecting recipes.';
 	_message = '';
 
-	_generateMarkup() {
-		console.log(this._data);
-		return this._data.map(this._generateMarupPreview).join('');
+	addHanderRender(handler) {
+		window.addEventListener('load', handler);
 	}
-
+	
 	_generateMarkup() {
-		const id = window.location.hash.slice(1);
-
-		return `
-          <li class="preview">
-            <a class="preview__link ${
-            	result.id === id ? 'preview__link--active' : ''
-          }" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-                </div>
-              </div>
-            </a>
-          </li>
-		`;
+		return this._data
+		.map(bookmark => previewView.render(bookmark, false))
+		.join('');
 	}
 }
 
